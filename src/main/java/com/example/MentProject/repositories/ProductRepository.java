692a28f8c3278c1d83db6fc5d9dc728e85032829
dzinais.kaliada <dvkoleda@gmail.com>
2023-09-12 19:44:36 +0200

    Added a lot of comments

--- src/main/java/com/example/MentProject/repositories/ProductRepository.java
+++ src/main/java/com/example/MentProject/repositories/ProductRepository.java
@@ -3,9 +3,7 @@ package com.example.MentProject.repositories;
 import com.example.MentProject.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
-
+// Only one empty line is allowed between imports and the first class declaration.
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
 }
+// Good! Each file should end with a single empty line. Why? See https://stackoverflow.com/a/729795/1157054
